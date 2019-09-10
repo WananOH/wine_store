cbd0911465334fe7d7f29c0ded7f55f0c4f3a6ef
wesley <wesley.chen24@foxmail.com>
2019-09-10 17:10:47 +0800

    分销奖励

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -10,7 +10,6 @@ class UserController extends Controller{
 
     public function index()
     {
-
         $user = auth()->user();
         $user = collect($user)->only('name','nickname','avatar','phone','email','qrcode');
 
--- app/Jobs/OrderRebate.php
+++ app/Jobs/OrderRebate.php
@@ -3,6 +3,8 @@
 namespace App\Jobs;
 
 use App\Models\Order;
+use App\Models\User;
+use App\Traits\Tree;
 use EasyWeChat\Factory;
 use Illuminate\Bus\Queueable;
 use Illuminate\Support\Facades\DB;
@@ -13,7 +15,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 
 class OrderRebate implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, Tree;
 
     protected $order;
 
@@ -38,18 +40,43 @@ class OrderRebate implements ShouldQueue
 
         $app = Factory::payment(config('wechat.payment.default'));
 
+        $users = User::get('id','openid','name','parent_id')->toArray();
+        $parents = $this->parents($users,$this->order->user->parent_id);
         //第一层
-        $app->transfer->toBalance([
-            'partner_trade_no' => '1233455',
-            'openid' => 'oxTWIuGaIt6gTKsQRLau2M0yL16E',
-            'check_name' => 'FORCE_CHECK',
-            're_user_name' => '王小帅',
-            'amount' => 10000,
-            'desc' => '理赔',
-        ]);
+        if(isset($parents[0])){
+            $app->transfer->toBalance([
+                'partner_trade_no' => '1233455',
+                'openid' => $parents[0]['openid'],
+                'check_name' => 'FORCE_CHECK',
+                're_user_name' => $parents[0]['name'],
+                'amount' => 10000,
+                'desc' => '理赔',
+            ]);
+        }
 
         //第二层
-
+        if(isset($parents[0])){
+            $app->transfer->toBalance([
+                'partner_trade_no' => '1233455',
+                'openid' => $parents[1]['openid'],
+                'check_name' => 'FORCE_CHECK',
+                're_user_name' => $parents[1]['name'],
+                'amount' => 10000,
+                'desc' => '理赔',
+            ]);
+        }
         //第三层
+        if(isset($parents[0])){
+            $app->transfer->toBalance([
+                'partner_trade_no' => '1233455',
+                'openid' => $parents[2]['openid'],
+                'check_name' => 'FORCE_CHECK',
+                're_user_name' => $parents[2]['name'],
+                'amount' => 10000,
+                'desc' => '理赔',
+            ]);
+        }
+
     }
+
 }
